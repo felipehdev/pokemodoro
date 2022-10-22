ba6162bbcc5a74676545bd08fbb273d3d1b70239
felipr <100290286+felipehdev@users.noreply.github.com>
2022-10-21 22:01:51 -0300

    pokemons sendo salvo em objetos preparados para impressao

--- pokemodoro/src/components/SavedPokemons/SavedPokemons.jsx
+++ pokemodoro/src/components/SavedPokemons/SavedPokemons.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
-const SavedPokemons = () => {
+const SavedPokemons = ({ requestedData }) => {
+
+
+  // cons que salvam os objetos antes de imprimir
+  const [toPrint, setToPrint] = useState([]);
+  console.log(toPrint);
+
+  //aqui sao requeridas todas a infos de cada pokemons salvo
+  function reqPokeInfo() {
+    requestedData.pokemons.map(
+      async (ids) => {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${ids}`);
+        const data = await response.json();
+        setToPrint((newToPrint) => [...newToPrint, data]);
+      }
+    )
+    
+  }
+
+
+  //funçao que imprime os pokes na tela
+
+
   return (
-    <div>SavedPokemons</div>
-  )
-}
+    <div>
+      <span>Seus pokemons salvos:</span>
+      <div>
+        <span>{requestedData.pokemons}</span>
+      </div>
+      <br />
+      <button onClick={()=> reqPokeInfo()}>ver</button>
 
-export default SavedPokemons
+    </div>
+  );
+};
+
+export default SavedPokemons;
